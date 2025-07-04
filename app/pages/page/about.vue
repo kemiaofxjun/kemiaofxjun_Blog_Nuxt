b32d1c8d21a25b348660b8e06e2f2e8e479afa7f
661111 <xiaoxin104@outlook.com>
2025-07-04 21:00:04 +0800

    添加关于页面的css

---
+++ app/pages/page/about.vue
@@ -0,0 +1,424 @@
+<template>
+    <div class="about-page">
+        <link href="/assets/css/about.css" rel="stylesheet">
+        <div class="author-main">
+            <div class="author-tag-left">
+                <span class="author-tag">
+                    💻 Like数码科技
+                </span>
+                <span class="author-tag">
+                    🥣 干饭魂 干饭人
+                </span>
+                <span class="author-tag">
+                    🕊 咕咕咕咕咕咕~
+                </span>
+                <span class="author-tag">
+                    🧱 CV工程师
+                </span>
+            </div>
+            <div class="author-box">
+                <span>
+                </span>
+                <div class="author-img">
+                    <img class="no-lightbox" src="https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg">
+                </div>
+            </div>
+            <div class="author-tag-right">
+                <span class="author-tag">
+                    吃饭不如碎觉 💤
+                </span>
+                <span class="author-tag">
+                    乐观 积极 向上 🤝
+                </span>
+                <span class="author-tag">
+                    专攻各种困难 🔨
+                </span>
+                <span class="author-tag">
+                    人不狠话超多 💢
+                </span>
+            </div>
+            <div class="image-dot">
+        </div>
+        <p class="p center logo large">
+		    关于本站
+	    </p>
+	<div class="author-content">
+		<div class="author-content-item myInfoAndSayHello">
+			<div class="title1">
+				你好，很高兴认识你👋
+			</div>
+			<div class="title2">
+				我叫
+				<span class="inline-word">
+					陈志伟
+				</span>
+			</div>
+			<div class="title1">
+				是一名 前端工程师、学生、独立开发者、
+				<span class="inline-word">
+					博主
+				</span>
+			</div>
+		</div>
+		<div class="aboutsiteTips author-content-item">
+			<div class="author-content-item-tips">
+				追求
+			</div>
+			<h2>
+				源于
+				<br/>
+				热爱而去
+				<span class="inline-word">
+					感受
+				</span>
+				<div class="mask">
+					<span class="first-tips">
+						学习
+					</span>
+					<span>
+						生活
+					</span>
+					<span data-up="data-up">
+						程序
+					</span>
+					<span data-show="data-show">
+						体验
+					</span>
+				</div>
+			</h2>
+		</div>
+    </div>
+    <div class="hello-about">
+		<div class="cursor" style="translate:none;rotate:none;scale:none;transform:translate(721px,180px)"></div>
+		<div class="shapes">
+			<div class="shape shape-1" style="translate:none;rotate:none;scale:none;transform:translate(721px,180px)"></div>
+			<div class="shape shape-2" style="translate:none;rotate:none;scale:none;transform:translate(721px,180px)"></div>
+			<div class="shape shape-3" style="translate:none;rotate:none;scale:none;transform:translate(721px,180px)"></div>
+		</div>
+		<div class="content">
+			<h1>Hello there!</h1>
+		</div>
+	</div>
+	<div class="author-content">
+		<div class="about-statistic author-content-item">
+			<div class="card-content">
+				<div class="author-content-item-tips">数据</div><span class="author-content-item-title">访问统计</span>
+				<div id="statistic"></div>
+				<div class="post-tips">统计信息来自<a href="https://invite.51.la/1NzKqTeb?target=V6" target="_blank" rel="noopener nofollow">51la网站统计</a></div>
+				<div class="banner-button-group"><a class="banner-button" onclick="pjax.loadUrl(&quot;/archives/&quot;)" data-pjax-state="data-pjax-state"><i class="fas fa-circle-right"></i><span class="banner-button-text">文章隧道</span></a></div>
+			</div>
+		</div>
+		<div class="author-content-item-group column mapAndInfo">
+			<div class="author-content-item map single"><span class="map-title">我现在住在<b>中国，长沙市</b></span></div>
+			<div class="author-content-item selfInfo single">
+				<div><span class="selfInfo-title">生于</span><span class="selfInfo-content" id="selfInfo-content-year" style="color:#43a6c6">2002</span></div>
+				<div><span class="selfInfo-title">湖南信息学院</span><span class="selfInfo-content" style="color:#c69043">软件工程</span></div>
+				<div><span class="selfInfo-title">现在职业</span><span class="selfInfo-content" style="color:#b04fe6"> </span></div>
+			</div>
+		</div>
+	</div>
+	<div class="author-content">
+		<div class="author-content-item maxim">
+			<div class="author-content-item-tips">座右铭</div><span class="maxim-title"><span style="opacity:.6;margin-bottom:8px">生活明朗，</span><span>万物可爱。</span></span>
+		</div>
+		<div class="author-content-item myphoto"><img class="author-content-img no-lightbox" alt="自拍" src="https://img02.anheyu.com/adminuploads/1/2022/09/24/632e9643611ec.jpg" /></div>
+	</div>
+	<div class="author-content">
+		<div class="author-content-item like-technology">
+			<div class="card-content">
+				<div class="author-content-item-tips">关注偏好</div><span class="author-content-item-title">数码科技</span>
+				<div class="content-bottom">
+					<div class="tips">手机、电脑软硬件</div>
+				</div>
+			</div>
+		</div>
+		<div class="author-content-item game-yuanshen">
+			<div class="card-content">
+				<div class="author-content-item-tips">爱好游戏</div><span class="author-content-item-title">原神</span>
+				<div class="content-bottom">
+					<div class="icon-group">
+						<div class="loading-bar" role="presentation" aria-hidden="true"><img class="no-lightbox" src="https://yuanshen.site/imgs/loading-bar.png" alt="Loading..." longdesc="https://ys.mihoyo.com/main/" /></div>
+					</div>
+					<div class="tips game-yuanshen-uid">UID: 125766904</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="author-content">
+		<div class="create-site-post author-content-item single">
+			<div class="author-content-item-tips">心路历程</div>欢迎来到我的博客 😝，这里是我记笔记的地方 🙌，目前就读于长沙<strong>湖南信息学院</strong>的<strong>软件工程</strong>专业，虽然有时候常常会忘记更新笔记，咕咕 ✋~ 但是记笔记真的是一个很棒的习惯 💪，能把学下来的知识进行积累，沉淀，有一句话说的好，能教给别人的知识，才是真正学会了的知识 ⚡ 每周我都会尽量进行更新 ☁️，如果没更的话，可能是我忘了，也可能是我在钻研某个东西的时候太入迷了
+		</div>
+	</div>
+</template>
+
+<style>
+/* 1.基础架构 */
+#about-page .author-main {
+  display: -webkit-box;
+  display: -moz-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: box;
+  display: flex;
+  -webkit-box-align: center;
+  -moz-box-align: center;
+  -o-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -moz-box-pack: center;
+  -o-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  margin: 0 0 16px 0;
+  user-select: none;
+}
+#about-page .author-box {
+  position: relative;
+  width: 189px;
+  height: 189px;
+  background: rgba(253, 253, 253, 0.8);
+  border-radius: 50%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#about-page .author-img {
+  margin: auto;
+  border-radius: 50%;
+  overflow: hidden;
+  width: 180px;
+  height: 180px;
+  z-index: 10;
+  background: var(--mj-card-bg);
+}
+.author-tag-left {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  margin-right: 30px;
+}
+.author-tag-right {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 30px;
+}
+/* 2.头像美化 */
+#about-page .author-box span {
+  position: absolute;
+  inset: 5px;
+  border-radius: 50%;
+  background: rgba(253, 253, 253, 0.8);
+  z-index: 1;
+}
+#about-page .author-box::before {
+  content: '';
+  position: absolute;
+  width: 500px;
+  height: 500px;
+  background-image: conic-gradient(transparent, transparent, transparent, #8758FF);
+  animation: animate 4s linear infinite;
+  animation-delay: -2s;
+}
+#about-page .author-box::after {
+  content: '';
+  position: absolute;
+  width: 500px;
+  height: 500px;
+  background-image: conic-gradient(transparent, transparent, transparent, #5CB8E4);
+  animation: animate 4s linear infinite;
+}
+/* 3.列表卡片美化 */
+.author-tag {
+  transform: translate(0, -4px);
+  padding: 1px 8px;
+  background: var(--heo-card-bg);
+  border: var(--style-border-always);
+  border-radius: 40px;
+  margin-top: 6px;
+  font-size: 14px;
+  font-weight: bold;
+  box-shadow: var(--heo-shadow-lightblack);
+  animation: 6s ease-in-out 0s infinite normal none running floating;
+}
+/* 4.列表卡片美化以及动画 */
+/* 4.1.左序列 */
+.author-tag-left .author-tag:first-child, .author-tag-left .author-tag:last-child {
+  margin-right: -16px;
+}
+/* 4.2.右序列 */
+.author-tag-right .author-tag:first-child, .author-tag-right .author-tag:last-child {
+  margin-left: -16px;
+}
+.author-tag:nth-child(1) {
+  animation-delay: 0s;
+}
+.author-tag:nth-child(2) {
+  animation-delay: 0.6s;
+}
+.author-tag:nth-child(3) {
+  animation-delay: 1.2s;
+}
+.author-tag:nth-child(4) {
+  animation-delay: 1.8s;
+}
+/* 5.动画css */
+@keyframes floating {
+  0% {
+    transform: translate(0, -4px);
+  }
+  50% {
+    transform: translate(0, 4px);
+  }
+  100% {
+    transform: translate(0, -4px);
+  }
+}
+@keyframes animate {
+  0% {
+    transform: rotate(0)
+  }
+  100% {
+    transform: rotate(360deg)
+  }
+}
+/* 6.关于本站文字样式美化 */
+[data-theme=dark] #about-page .author-title {
+  text-stroke: 1px #bccbe4;
+  -webkit-text-stroke: 1px #bccbe4;
+}
+#about-page .author-title {
+  font-size: 2.7rem;
+  font-weight: 700;
+  margin-top: 1px;
+  letter-spacing: 6px;
+  -webkit-background-clip: text;
+  background-image: linear-gradient(90deg, #2ca2b4, #5598de 24%, #7f87ff 45%, #f65aad 85%, #df80b4);
+  background-clip: text;
+  display: inline-block;
+  color: transparent;
+  text-stroke: 2px #3fdaee;
+  -webkit-text-stroke: 1px #3fdaee;
+}
+</style>
+
+<script src="https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/gsap/3.9.1/gsap.min.js"></script>
+<script>
+	(() => {
+    function isInViewPortOfOne(el) {
+        if (!el) return;
+        const viewPortHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        const offsetTop = el.offsetTop;
+        const scrollTop = document.documentElement.scrollTop;
+        const top = offsetTop - scrollTop;
+        return top <= viewPortHeight;
+    }
+    fetch('https://v6-widget.51.la/v6/527574/quote.js').then(res => res.text()).then((data) => {
+        let title = ['最近活跃', '今日人数', '今日访问', '昨日人数', '昨日访问', '本月访问', '总访问量']
+        let num = data.match(/(<\/span><span>).*?(\/span><\/p>)/g)
+
+        num = num.map((el) => {
+            let val = el.replace(/(<\/span><span>)/g, '')
+            let str = val.replace(/(<\/span><\/p>)/g, '')
+            return str
+        })
+
+        let statisticEl = document.getElementById('statistic')
+
+        // 自定义不显示哪个或者显示哪个，如下为不显示 最近活跃访客 和 总访问量
+        let statistic = []
+        for (let i = 0; i < num.length; i++) {
+            if (!statisticEl) return
+            if (i == 0 || i == num.length - 1) continue;
+            statisticEl.innerHTML += '<div><span>' + title[i] + '</span><span id=' + title[i] + '>' + num[i] + '</span></div>'
+            queueMicrotask(() => {
+                statistic.push(new CountUp(title[i], 0, num[i], 0, 2, {
+                    useEasing: true,
+                    useGrouping: true,
+                    separator: ',',
+                    decimal: '.',
+                    prefix: '',
+                    suffix: ''
+                }))
+            })
+        }
+
+        function statisticUP() {
+            let statisticElment = document.querySelector('.about-statistic.author-content-item');
+            if (isInViewPortOfOne(statisticElment)) {
+                for (let i = 0; i < num.length; i++) {
+                    if (i == 0 || i == num.length - 1) continue;
+                    statistic[i - 1].start();
+                }
+                document.removeEventListener('scroll', throttleStatisticUP);
+            }
+        }
+
+        const selfInfoContentYear = new CountUp('selfInfo-content-year', 0, 2002, 0, 2, {
+            useEasing: true,
+            useGrouping: false,
+            separator: ',',
+            decimal: '.',
+            prefix: '',
+            suffix: ''
+        })
+
+        function scrollSelfInfoContentYear() {
+            let selfInfoContentYearElment = document.querySelector('.author-content-item.selfInfo.single');
+            if (selfInfoContentYearElment && isInViewPortOfOne(selfInfoContentYearElment)) {
+                selfInfoContentYear.start()
+                document.removeEventListener('scroll', throttleScrollSelfInfoContentYear);
+            }
+        }
+        const throttleStatisticUP = btf.throttle(statisticUP, 200)
+        document.addEventListener('scroll', throttleStatisticUP, {
+            passive: true
+        })
+
+        const throttleScrollSelfInfoContentYear = btf.throttle(scrollSelfInfoContentYear, 200)
+        document.addEventListener('scroll', throttleScrollSelfInfoContentYear, {
+            passive: true
+        })
+    });
+
+    var pursuitInterval = null;
+    pursuitInterval = setInterval(function() {
+        const show = document.querySelector('span[data-show]')
+        const next = show.nextElementSibling || document.querySelector('.first-tips')
+        const up = document.querySelector('span[data-up]')
+
+        if (up) {
+            up.removeAttribute('data-up')
+        }
+
+        show.removeAttribute('data-show')
+        show.setAttribute('data-up', '')
+
+        next.setAttribute('data-show', '')
+    }, 2000)
+
+    document.addEventListener('pjax:send', function() {
+        clearInterval(pursuitInterval);
+    });
+
+    var helloAboutEl = document.querySelector('.hello-about')
+    helloAboutEl.addEventListener("mousemove", evt => {
+        const mouseX = evt.offsetX;
+        const mouseY = evt.offsetY;
+        gsap.set(".cursor", {
+            x: mouseX,
+            y: mouseY,
+        })
+
+        gsap.to(".shape", {
+            x: mouseX,
+            y: mouseY,
+            stagger: -0.1
+        })
+    })
+})()
+</script>
