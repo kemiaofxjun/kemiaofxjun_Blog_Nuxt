47ae0145a748ba11a563ad1084f4cee7c0023a38
661111 <xiaoxin104@outlook.com>
2025-07-04 16:31:56 +0800

    删除部分统计地址，防止出现跨域明文

--- blog.config.ts
+++ blog.config.ts
@@ -49,8 +49,6 @@ const blogConfig = {
     scripts: [
         // 自己部署的 Umami 统计服务
         { 'src': 'https://zhi.zhilu.cyou/zhi.js', 'data-website-id': 'a1997c81-a42b-46f6-8d1d-8fbd67a8ef41', 'defer': true },
-        // Cloudflare Insights 统计服务
-        { 'src': 'https://static.cloudflareinsights.com/beacon.min.js', 'data-cf-beacon': '{"token": "97a4fe32ed8240ac8284e9bffaf03962"}', 'defer': true },
     ],
 
     // 用于 Shiki、Plain Shiki 引入代码高亮
