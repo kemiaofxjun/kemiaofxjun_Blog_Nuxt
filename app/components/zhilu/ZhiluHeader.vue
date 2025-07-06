c99be21267860ad37bd2889881ebee5e5c143262
661111 <xiaoxin104@outlook.com>
2025-07-06 13:01:53 +0800

    添加字体并修改，测试

--- app/components/zhilu/ZhiluHeader.vue
+++ app/components/zhilu/ZhiluHeader.vue
@@ -58,13 +58,7 @@ const appConfig = useAppConfig()
     }
 }
 
-@font-face {
-    font-family: AlimamaFangYuanTi;
-    src: url("/fonts/AlimamaFangYuanTi.woff2");
-}
-
 .header-title {
-    font-family: AlimamaFangYuanTi;
     font-size: 1.5em;
     font-synthesis: none;
     font-variation-settings: "wght" 600, "BEVL" 100;
