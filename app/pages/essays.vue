f41fa4fd038ce7ffaec502b2304ed7547065a627
661111 <xiaoxin104@outlook.com>
2025-07-06 17:58:56 +0800

    修改说说页面各种内容块

--- app/pages/essays.vue
+++ app/pages/essays.vue
@@ -38,36 +38,41 @@ useSeoMeta({
         </div>
         <div id="ispeak">
         </div>
-        <link rel="stylesheet" href="https://cdn.staticfile.org/highlight.js/10.6.0/styles/atom-one-dark.min.css"/>
-        <link rel="stylesheet" href="https://jsd.myxz.top/npm/ispeak@4.4.0/style.css"/>
-
+        <link 
+            rel="stylesheet" 
+            href="https://cdn.staticfile.org/highlight.js/10.6.0/styles/atom-one-dark.min.css"
+        />
+        <link
+            rel="stylesheet"
+            href="https://jsd.myxz.top/npm/ispeak@4.4.0/style.css"
+        />
         <script src="https://cdn.staticfile.org/highlight.js/10.6.0/highlight.min.js"></script>
         <script src="https://cdn.staticfile.org/marked/2.0.0/marked.min.js"></script>
         <script src="https://jsd.myxz.top/npm/ispeak@4.4.0/ispeak.umd.js"></script>
-        <script>
-            var head = document.getElementsByTagName('head')[0]
-            var meta = document.createElement('meta')
-            meta.name = 'referrer'
-            meta.content = 'no-referrer'
-            head.appendChild(meta)
-            if (ispeak) {
-                ispeak
-                .init({
-                    el: '#ispeak',
-                    api: 'https://kkapi.myxz.top/',
-                    author: '61fe93508fd621d39a155725',
-                    pageSize: 10,
-                    loading_img: 'https://bu.dusays.com/2021/03/04/d2d5e983e2961.gif'
-                })
-                .then(function () {
-                    console.log('ispeak 加载完成')
-                    document.getElementById('tip').style.display = 'none'
-                })
-            } else {
-                document.getElementById('tip').innerHTML = 'ipseak依赖加载失败！'
-            }
-        </script>
     </div>
+<script>
+  var head = document.getElementsByTagName('head')[0]
+  var meta = document.createElement('meta')
+  meta.name = 'referrer'
+  meta.content = 'no-referrer'
+  head.appendChild(meta)
+  if (ispeak) {
+    ispeak
+      .init({
+        el: '#ispeak',
+        api: 'https://kkapi.myxz.top/',
+        author: '61fe93508fd621d39a155725',
+        pageSize: 10,
+        loading_img: 'https://bu.dusays.com/2021/03/04/d2d5e983e2961.gif'
+      })
+      .then(function () {
+        console.log('ispeak 加载完成')
+        document.getElementById('tip').style.display = 'none'
+      })
+  } else {
+    document.getElementById('tip').innerHTML = 'ipseak依赖加载失败！'
+  }
+</script>
 </template>
 
 <style>
