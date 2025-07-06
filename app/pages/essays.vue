d0366dd8e66a1bb829ccf3506da61ead1a595d52
661111 <xiaoxin104@outlook.com>
2025-07-06 22:51:54 +0800

    修改说说页面各种内容块

--- app/pages/essays.vue
+++ app/pages/essays.vue
@@ -52,30 +52,6 @@ useSeoMeta({
     href="https://cdn.jsdmirror.com/npm/@waline/client/dist/waline.css"
     />
     <link rel="stylesheet" href="https://cdn.jsdmirror.com/npm/@waline/client/dist/waline-meta.css"/>
-    <script>
-    var head = document.getElementsByTagName('head')[0]
-    var meta = document.createElement('meta')
-    meta.name = 'referrer'
-    meta.content = 'no-referrer'
-    head.appendChild(meta)
-    if (ispeak) {
-        ispeak
-        .init({
-            el: '#ispeak',
-            api: 'https://kkapi.ccknbc.cc/',
-            author: '621cd42048c49d6f96787626',
-            pageSize: 10,
-            loading_img: 'https://bu.dusays.com/2022/05/01/626e88f349943.gif',
-            speakPage: '/essay'
-        })
-        .then(function () {
-            console.log('ispeak 加载完成')
-            document.getElementById('tip').style.display = 'none'
-        })
-    } else {
-        document.getElementById('tip').innerHTML = 'ipseak依赖加载失败！'
-    }
-    </script>
 </template>
 
 <style>
@@ -168,3 +144,69 @@ useSeoMeta({
     opacity: .8
 }
 </style>
+
+<script setup>
+  var head = document.getElementsByTagName('head')[0]
+  var meta = document.createElement('meta')
+  meta.name = 'referrer'
+  meta.content = 'no-referrer'
+  head.appendChild(meta)
+  if (ispeak) {
+    ispeak
+      .init({
+        el: '#ispeak',
+        api: 'https://kkapi.ccknbc.cc/',
+        author: '621cd42048c49d6f96787626',
+        pageSize: 10,
+        loading_img: 'https://bu.dusays.com/2022/05/01/626e88f349943.gif',
+        speakPage: '/essay',
+        githubClientId: 'Iv1.f333c02f5f6676e8',
+        comment: function (speak) {
+          // 4.4.0 之后在此回调函数中初始化评论
+          const { _id, title, content } = speak
+          const contentSub = content.substring(0, 30)
+          import("https://cdn.jsdmirror.com/npm/@waline/client/dist/waline.js").then((Waline) => {
+            Waline.init({
+              el: '.ispeak-comment', // 默认情况下 ipseak 生成class为 ispeak-comment 的div
+              path: '/essay/speak?q=' + _id, // 手动传入当前speak的唯一id
+              title: title || contentSub, // 手动传入当前speak的标题(由于content可能过长，因此截取前30个字符)
+              serverURL: 'https://waline.ccknbc.cc',
+              pageSize: 10,
+              requiredMeta: ["nick", "mail"],
+              login: 'force',
+              dark: 'html[data-theme="dark"]',
+              imageUploader: function (file) {
+                let formData = new FormData();
+                let headers = new Headers();
+                formData.append("file", file);
+                formData.append("album_id", "10");
+                formData.append("permission", "0");
+                headers.append("Authorization", "Bearer 24|o8Crl5y0oK3luyUs17fBxDtAcevk1iiLHVFMNjpA");
+                headers.append("Accept", "application/json");
+                return fetch("https://wmimg.com/api/v1/upload", {
+                  method: "POST",
+                  headers: headers,
+                  body: formData,
+                })
+                  .then((resp) => resp.json())
+                  .then((resp) => resp.data.links.url);
+                },
+              turnstileKey: '0x4AAAAAAAECBl27OB5SZrQT',
+              emoji:
+                [
+                  "https://cdn.jsdmirror.com/npm/sticker-heo/Sticker-100/",
+                  // "https://cdn.jsdmirror.com/npm/telegram-gif/Telegram-Gif/",
+                  // "https://cdn.jsdmirror.com/npm/@waline/emojis/tw-emoji/"
+                ]
+            })
+          }).catch(error => console.error('Waline加载失败', error));
+        }
+      })
+      .then(function () {
+        console.log('ispeak 加载完成')
+        document.getElementById('tip').style.display = 'none'
+      })
+  } else {
+    document.getElementById('tip').innerHTML = 'ipseak依赖加载失败！'
+  }
+</script>
