e02ea1031e8ebc661b80bbb6faee1e9888d97735
661111 <xiaoxin104@outlook.com>
2025-07-12 15:21:55 +0800

    修改service worker文件链接

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,6 @@
     <ZSidebar />
     <div id="content">
         <main id="main-content">
-            <script src="/assets/js/pwa_update.js"></script>
             <NuxtPage />
             <ZFooter />
         </main>
