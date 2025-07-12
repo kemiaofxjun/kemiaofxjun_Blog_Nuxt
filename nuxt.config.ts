26d0bfa95055cbb4792225decbbea75744af8a10
661111 <xiaoxin104@outlook.com>
2025-07-12 15:32:24 +0800

    修改service worker配置文件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -172,9 +172,9 @@ export default defineNuxtConfig({
         injectRegister: false,
         includeManifestIcons: false,
         manifest: false,
-        maximumFileSizeToCacheInBytes: 3000000,
         injectManifest: {
             globPatterns: ['**/*.{js,json,css,html,txt,svg,png,ico,webp,woff,woff2,ttf,eot,otf,wasm}'],
+            maximumFileSizeToCacheInBytes: 3000000,
         },
         devOptions: {
             enabled: true,
