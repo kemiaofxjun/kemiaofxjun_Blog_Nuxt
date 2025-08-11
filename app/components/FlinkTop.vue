7e6259850e375fb939545bdd541cb2f6c0a77ba8
661111 <xiaoxin104@outlook.com>
2025-08-11 09:09:23 +0800

    更新友链页面组件

--- app/components/FlinkTop.vue
+++ app/components/FlinkTop.vue
@@ -35,11 +35,11 @@ const bannerInfo = [
     </div>
     <div id="skills-tags-group-all">
       <div class="tags-group-wrapper" v-for="groups in friendsInfo" :key="groups.name">
-        <div class="tags-group-icon-pair" style="margin-left:1rem" v-for="flink in groups.entries" :key="flink.author" >
-          <a class="tags-group-icon no-text-decoration" target="_blank" rel="noopener" :href="flink.link" :title="flink.author" :key="flink.author">
+        <div class="tags-group-icon-pair" style="margin-left:1rem" v-for="flink in groups.entries" :key="flink.date" >
+          <a class="tags-group-icon no-text-decoration" target="_blank" rel="noopener" :href="flink.link" :title="flink.author">
             <img class="no-lightbox" :title="flink.author" :src="flink.avatar" :alt="flink.author">
           </a>
-          <a class="tags-group-icon no-text-decoration" target="_blank" rel="noopener" :href="flink.link" :title="flink.author" :key="flink.author">
+          <a class="tags-group-icon no-text-decoration" target="_blank" rel="noopener" :href="flink.link" :title="flink.author">
             <img class="no-lightbox" :title="flink.author" :src="flink.avatar" :alt="flink.author">
           </a>
         </div>
