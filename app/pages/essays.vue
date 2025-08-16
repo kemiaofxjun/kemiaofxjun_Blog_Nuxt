d9a4c6dc37b82b469d26771597cb795ab25e477b
661111 <xiaoxin104@outlook.com>
2025-08-16 08:46:00 +0800

    更新版本

--- app/pages/essays.vue
+++ app/pages/essays.vue
@@ -294,6 +294,32 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.zone_imgbox {
+    display: grid;
+    gap: 8px;
+    grid-template-columns: repeat(3, 1fr);
+}
+
+.img-item {
+    border-radius: 8px;
+    overflow: hidden;
+    padding-bottom: 100%;
+    position: relative;
+}
+
+.image {
+    height: 100%;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    -o-object-fit: cover;
+    object-fit: cover;
+    position: absolute;
+    transition: transform .3s;
+    width: 100%;
+}
+
 .essay-stats {
     align-items: flex-end;
     color: #eee;
@@ -624,29 +650,5 @@ onMounted(() => {
     to {
         transform: rotate(1turn)
     }
-}
-
-.zone_imgbox {
-    display: grid;
-    gap: 8px;
-    grid-template-columns: repeat(3, 1fr);
-}
-.img-item {
-    border-radius: 8px;
-    overflow: hidden;
-    padding-bottom: 100%;
-    position: relative;
-}
-.image {
-    height: 100%;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    -o-object-fit: cover;
-    object-fit: cover;
-    position: absolute;
-    transition: transform .3s;
-    width: 100%;
 }
 </style>
