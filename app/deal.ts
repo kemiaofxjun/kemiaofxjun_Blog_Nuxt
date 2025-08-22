5ee7db02ab78fcb7da4ba68506a4f3cf8dcca72d
lumianya <mcysw0815@gmail.com>
2025-08-22 22:59:17 +0800

    修改配置

--- app/deal.ts
+++ app/deal.ts
@@ -10,9 +10,9 @@ export interface dealMiniLogo {
 }
 
 export const dealData: dealLeft[] =  [{
-    { icon: 'ph:envelope-simple-bold', name: '邮箱', link: 'mailto:3227988255@qq.com', }
+    { icon: 'ph:envelope-simple-bold', name: '邮箱', link: 'mailto:me@mail.kemeow.top', }
 }]
 
 export const miniLogo: dealMiniLogo[] = [{
-    { name:'莫言小栈', icon: 'https://blog.myxz.top/img/avatar.avif' }
+    { name:'喵洛阁', icon: 'https://img.314926.xyz/images/2025/08/13/no-background-kemiaofxjun.webp' }
 }]
