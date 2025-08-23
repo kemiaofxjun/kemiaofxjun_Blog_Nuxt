b4ab36ee1bec8d4ea0e1be38edbf13c3ca3d26fb
661111 <xiaoxin104@outlook.com>
2025-08-23 17:43:21 +0800

    更新页面以及样式

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -174,3 +174,158 @@ layoutStore.setAside(['blog-stats', 'connectivity', 'latest-comments', 'blog-log
         </div>
     </div>
 </template>
+
+<style lang="css" scoped>
+  /* 1.基础架构 */
+  #about-page .author-main {
+    display: -webkit-box;
+    display: -moz-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: box;
+    display: flex;
+    -webkit-box-align: center;
+    -moz-box-align: center;
+    -o-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -moz-box-pack: center;
+    -o-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    margin: 0 0 16px 0;
+    user-select: none;
+  }
+  #about-page .author-box {
+    position: relative;
+    width: 189px;
+    height: 189px;
+    background: rgba(253, 253, 253, 0.8);
+    border-radius: 50%;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #about-page .author-img {
+    margin: auto;
+    border-radius: 50%;
+    overflow: hidden;
+    width: 180px;
+    height: 180px;
+    z-index: 10;
+    background: var(--mj-card-bg);
+  }
+  .author-tag-left {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin-right: 30px;
+  }
+  .author-tag-right {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 30px;
+  }
+  /* 2.头像美化 */
+  #about-page .author-box span {
+    position: absolute;
+    inset: 5px;
+    border-radius: 50%;
+    background: rgba(253, 253, 253, 0.8);
+    z-index: 1;
+  }
+  #about-page .author-box::before {
+    content: '';
+    position: absolute;
+    width: 500px;
+    height: 500px;
+    background-image: conic-gradient(transparent, transparent, transparent, #8758FF);
+    animation: animate 4s linear infinite;
+    animation-delay: -2s;
+  }
+  #about-page .author-box::after {
+    content: '';
+    position: absolute;
+    width: 500px;
+    height: 500px;
+    background-image: conic-gradient(transparent, transparent, transparent, #5CB8E4);
+    animation: animate 4s linear infinite;
+  }
+  /* 3.列表卡片美化 */
+  .author-tag {
+    transform: translate(0, -4px);
+    padding: 1px 8px;
+    background: var(--heo-card-bg);
+    border: var(--style-border-always);
+    border-radius: 40px;
+    margin-top: 6px;
+    font-size: 14px;
+    font-weight: bold;
+    box-shadow: var(--heo-shadow-lightblack);
+    animation: 6s ease-in-out 0s infinite normal none running floating;
+  }
+  /* 4.列表卡片美化以及动画 */
+  /* 4.1.左序列 */
+  .author-tag-left .author-tag:first-child, .author-tag-left .author-tag:last-child {
+    margin-right: -16px;
+  }
+  /* 4.2.右序列 */
+  .author-tag-right .author-tag:first-child, .author-tag-right .author-tag:last-child {
+    margin-left: -16px;
+  }
+  .author-tag:nth-child(1) {
+    animation-delay: 0s;
+  }
+  .author-tag:nth-child(2) {
+    animation-delay: 0.6s;
+  }
+  .author-tag:nth-child(3) {
+    animation-delay: 1.2s;
+  }
+  .author-tag:nth-child(4) {
+    animation-delay: 1.8s;
+  }
+  /* 5.动画css */
+  @keyframes floating {
+    0% {
+      transform: translate(0, -4px);
+    }
+    50% {
+      transform: translate(0, 4px);
+    }
+    100% {
+      transform: translate(0, -4px);
+    }
+  }
+  @keyframes animate {
+    0% {
+      transform: rotate(0)
+    }
+    100% {
+      transform: rotate(360deg)
+    }
+  }
+  /* 6.关于本站文字样式美化 */
+  [data-theme=dark] #about-page .author-title {
+    text-stroke: 1px #bccbe4;
+    -webkit-text-stroke: 1px #bccbe4;
+  }
+  #about-page .author-title {
+    font-size: 2.7rem;
+    font-weight: 700;
+    margin-top: 1px;
+    letter-spacing: 6px;
+    -webkit-background-clip: text;
+    background-image: linear-gradient(90deg, #2ca2b4, #5598de 24%, #7f87ff 45%, #f65aad 85%, #df80b4);
+    background-clip: text;
+    display: inline-block;
+    color: transparent;
+    text-stroke: 2px #3fdaee;
+    -webkit-text-stroke: 1px #3fdaee;
+  }
+</style>
