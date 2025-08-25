f1165e3e6e4c14e19ef4f7adffbf0693d0164d95
661111 <xiaoxin104@outlook.com>
2025-08-25 16:07:33 +0800

    更新组件逻辑

--- app/about.ts
+++ app/about.ts
@@ -107,7 +107,7 @@ export const about: aboutConnect[] = [
                     tag3: "🕊 咕咕咕咕咕咕~",
                     tag4: "🧱 CV工程师"
                 }],
-                logo: "/avatar.avif",
+                logo: "https://www.myxz.top/avatar.avif",
                 right: [{
                     tag1: "吃饭不如碎觉 💤",
                     tag2: "乐观 积极 向上 🤝",
