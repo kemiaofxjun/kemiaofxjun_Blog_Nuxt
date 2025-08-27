94c27f345519b9f83425701547dc25d0237fd155
661111 <xiaoxin104@outlook.com>
2025-08-27 11:12:18 +0800

    更新部分内容

--- app/components/post/Comment.vue
+++ app/components/post/Comment.vue
@@ -37,93 +37,31 @@ onMounted(() => {
 	}
 }
 
-:deep(#twikoo) {
+:root {
+	--ark-color-bg: #fff
+}
+
+.dark {
+	--ark-color-bg: #000000
+}
+
+:deep(#artalk) {
 	margin: 2em 0;
 
-	.tk-admin-container {
-		position: fixed;
-		z-index: 1;
-	}
+	.atk-main-editor {
+		background: var(--ark-color-bg)!important;
 
-	.tk-input {
-		font-family: var(--font-monospace);
-	}
-
-	.tk-time {
-		color: var(--c-text-3);
-	}
-
-	.tk-main {
-		margin-top: -0.1rem;
-	}
-
-	.tk-content {
-		margin-top: 0.1rem;
-
-		img {
-			border-radius: 0.5em;
+		.atk-main-editor>.atk-header input {
+			color: var(--ark-color-bg)!important;
 		}
-	}
-
-	.tk-comments-title, .tk-nick > strong {
-		font-family: var(--font-creative);
-	}
-
-	pre {
-		border-radius: 0.5rem;
-		font-size: 0.8125rem;
-	}
-
-	p {
-		margin: 0.2em 0;
-	}
-
-	menu, ol, ul {
-		margin: 0.5em 0;
-		padding: 0 0 0 1.5em;
-		list-style: revert;
-
-		> li {
-			margin: 0.2em 0;
-
-			&::marker {
-				font-size: 0.8em;
-				color: var(--c-primary);
+		.atk-textarea-wrap {
+			textarea.atk-textarea {
+				background: var(--ark-color-bg)!important;
 			}
 		}
-	}
-
-	blockquote {
-		margin: 0.5em 0;
-		padding: 0.2em 0.5em;
-		border-left: 4px solid var(--c-border);
-		border-radius: 4px;
-		background-color: var(--c-bg-2);
-		font-size: 0.9rem;
-
-		> .z-codeblock {
-			margin: 0 -0.8rem;
+		.atk-main-editor>.atk-bottom .atk-plug-btn i {
+			color: #ffffff;
 		}
-	}
-
-	.tk-owo-emotion {
-		width: auto;
-		height: 1.4em;
-		vertical-align: text-bottom;
-	}
-
-	.tk-extras, .tk-footer {
-		font-size: 0.7rem;
-		color: var(--c-text-3);
-	}
-
-	.tk-replies:not(.tk-replies-expand) {
-		mask: linear-gradient(#FFF 50%, transparent);
-	}
-
-	.tk-expand {
-		border-radius: 0.5rem;
-		transition: background-color 0.1s;
 	}
 }
 </style>
