7a6fe259c42bacdcad8e8d9595da61e6fbd8d577
661111 <xiaoxin104@outlook.com>
2025-08-29 10:22:57 +0800

    更新友链

--- app/pages/podcast.vue
+++ app/pages/podcast.vue
@@ -1,18 +1,20 @@
 <script lang="ts" setup>
-import { siteLinkItems } from '~/siteCard'
 import SiteLinkCard from '../components/card/siteLinkCard.vue';
 </script>
 
 <template>
 <div class="sitelinkHead">
-	<h2> 其他站点 </h2>
+	<h2> 站点 </h2>
 </div>
 
-<Tab :tabs="[siteLinkItems.name]" center>
+<Tab :tabs="['服务', '分站', '推荐']">
 	<template #tab1>
 		<SiteLinkCard />
 	</template>
 	<template #tab2>
+		<SiteLinkCard />
+	</template>
+	<template #tab3>
 		<SiteLinkCard />
 	</template>
 </Tab>
