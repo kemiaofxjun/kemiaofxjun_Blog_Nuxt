052d692e9ca51f871d823cb6c41e47c8dfc44cb9
661111 <xiaoxin104@outlook.com>
2025-08-29 08:53:37 +0800

    更新友链

---
+++ app/siteCard.ts
@@ -0,0 +1,15 @@
+import type { siteTabs } from '~/types/site'
+
+export default [
+    {
+        name: '',
+        siteItems: [{
+            name: '莫言小栈',
+            image: 'https://sourceimage.s3.bitiful.net/myxz.avif',
+            link: 'https://www.myxz.top',
+            service: [
+                { name: 'EdgeOne', image: ''}
+            ]
+        }]
+    }
+] satisfies siteTabs[]
